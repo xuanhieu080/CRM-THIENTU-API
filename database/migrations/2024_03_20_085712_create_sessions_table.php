47dc271c9987925ff69a4e10511be6422150f780
Hieu Phan Xuan <xuanhieu@Hieus-MacBook-Pro.local>
2024-03-20 09:26:34 +0700

    Update

--- database/migrations/2024_03_20_085712_create_sessions_table.php
+++ database/migrations/2024_03_20_085712_create_sessions_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+//        Schema::create('sessions', function (Blueprint $table) {
+//            $table->string('id')->primary();
+//            $table->foreignId('user_id')->nullable()->index();
+//            $table->string('ip_address', 45)->nullable();
+//            $table->text('user_agent')->nullable();
+//            $table->longText('payload');
+//            $table->integer('last_activity')->index();
+//        });
     }
 
     /**
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sessions');
+//        Schema::dropIfExists('sessions');
     }
 };
