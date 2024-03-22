31144f333a280c572eac7ede0b532d5c6085beea
Hieu Phan Xuan <xuanhieu@Hieus-MacBook-Pro.local>
2024-03-22 15:08:55 +0700

    Update

--- app/V1/API/Resources/CustomerResource.php
+++ app/V1/API/Resources/CustomerResource.php
@@ -26,6 +26,7 @@ class CustomerResource extends JsonResource
             'position_name'     => $this->position_name,
             'message'           => $this->message,
             'last_updated_at'   => $this->last_updated_at,
+            'created_at'        => $this->created_at,
             'contact_funnel_id' => $this->contact_funnel_id,
             'contact_funnel'    => new ContactFunnelResource($this->contactFunnel),
             'contact_source_id' => $this->contact_source_id,
